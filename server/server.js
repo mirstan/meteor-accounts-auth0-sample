cb44cad37ef705d3f19590014e5d7b70a57d819c
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2014-02-04 12:58:52 -0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+// All Tomorrow's Parties -- server
+
+Meteor.publish("directory", function () {
+  return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
+});
+
+Meteor.publish("parties", function () {
+  return Parties.find(
+    {$or: [{"public": true}, {invited: this.userId}, {owner: this.userId}]});
+});
+
+Accounts.loginServiceConfiguration.remove({
+  service: 'auth0'
+});
+
+Accounts.loginServiceConfiguration.insert({
+  service:      'auth0',
+  domain:       'contoso.auth0.com',
+  clientId:     'w560wy7zE4S3VRVA9SAkhy4N7E6hhZxJ',
+  clientSecret: '2HbTrXcXoTAMdHM_SsN3EBQ6DMbYvwExVGZIXF6Kmzce1tuIAU7MxnT2yUnqifkh'
+});
