6ce5388be68dbd1e249317ab7136b13c5d69100b
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2014-02-25 17:22:04 -0300

    updated to meteor 0.7.1.1

--- server/server.js
+++ server/server.js
@@ -9,11 +9,8 @@ Meteor.publish("parties", function () {
     {$or: [{"public": true}, {invited: this.userId}, {owner: this.userId}]});
 });
 
-Accounts.loginServiceConfiguration.remove({
-  service: 'auth0'
-});
-
-Accounts.loginServiceConfiguration.insert({
+ServiceConfiguration.configurations.remove({ service: 'auth0' });
+ServiceConfiguration.configurations.insert({
   service:      'auth0',
   domain:       'contoso.auth0.com',
   clientId:     'w560wy7zE4S3VRVA9SAkhy4N7E6hhZxJ',
